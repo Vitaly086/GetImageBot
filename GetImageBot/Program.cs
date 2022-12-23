3ab4162ba58edc29ca8b03f2c260a461f0f0b987
vitaly <shcherbinovv@gmai.com>
2022-12-23 17:42:28 +0600

    Revert "initial commit"
    This reverts commit c69fd3ce35ccb94fe2e92b6db57879a49d361156.

--- GetImageBot/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        var flickrApi = new FlickrAPI("c77b1b60abbb5751a82a01e545280c8d");
-        var bot = new Bot("5899586717:AAFHiuFz6Hp5hl4h150jkqNzWZIvvd5cCkk", flickrApi);
-    }
-}
